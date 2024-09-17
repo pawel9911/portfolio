65abb58e002cc63d22fba65de7609953f96c51bb
Paweł Gr <pawel.grzybek@l4web.pl>
2024-09-17 22:49:22 +0200

    feat(navigation): add paths

---
+++ src/router/paths.ts
@@ -0,0 +1,8 @@
+export const paths = {
+  root: "/",
+  dashboard: "/portfolio",
+  home: "/portfolio/home",
+  resume: "/portfolio/resume",
+  projects: "/portfolio/projects",
+  contact: "/portfolio/contact",
+} as const;
