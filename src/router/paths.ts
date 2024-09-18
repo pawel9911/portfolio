e5a6c42b254ddcc2c1345f090e517b2619fdc8f6
Paweł Gr <pawel.grzybek@l4web.pl>
2024-09-18 12:08:24 +0200

    feat(navigation): add path labels

--- src/router/paths.ts
+++ src/router/paths.ts
@@ -6,3 +6,14 @@ export const paths = {
   projects: "/portfolio/projects",
   contact: "/portfolio/contact",
 } as const;
+
+export type KnownPath = (typeof paths)[keyof typeof paths];
+
+export const pathLabels: Record<KnownPath, string> = {
+  [paths.root]: "",
+  [paths.dashboard]: "",
+  [paths.home]: "Home",
+  [paths.resume]: "Resume",
+  [paths.projects]: "Projects",
+  [paths.contact]: "Contact",
+};
