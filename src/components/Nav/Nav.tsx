5959302bd74ceca27a35b3a12c3d7d3ea767a57b
Paweł Gr <pawel.grzybek@l4web.pl>
2024-09-18 12:09:29 +0200

    feat(navigation): use hook in navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,22 +1,26 @@
-import { Link } from "react-router-dom";
-import { paths } from "../../router/paths";
+import { Link, useLocation } from "react-router-dom";
+import { useAccessibleNavigationPaths } from "../../router/utils";
 
 const Nav = () => {
+  const accessiblePaths = useAccessibleNavigationPaths();
+  const location = useLocation();
+
   return (
     <nav>
       <ul className="flex gap-8">
-        <li>
-          <Link to={paths.home}>Home</Link>
-        </li>
-        <li>
-          <Link to={paths.resume}>Resume</Link>
-        </li>
-        <li>
-          <Link to={paths.projects}>Projects</Link>
-        </li>
-        <li>
-          <Link to={paths.contact}>Contact</Link>
-        </li>
+        {accessiblePaths.map(({ isVisibleOnNavigation, name, path }) =>
+          isVisibleOnNavigation ? (
+            <li
+              key={path}
+              className={`${
+                location.pathname === path &&
+                "text-red-600 border-b-2 border-red-600"
+              } font-medium hover:text-red-600 transition-all`}
+            >
+              <Link to={path}>{name}</Link>
+            </li>
+          ) : null
+        )}
       </ul>
     </nav>
   );
