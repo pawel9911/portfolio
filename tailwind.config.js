78ffa7d29e4b862f2ba4967466f32a5eea76bd25
Paweł Gr <pawel.grzybek@l4web.pl>
2024-09-18 14:16:29 +0200

    feat(navigation): add text and background color to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
       fontFamily: {
         primary: ["Inconsolata", "monospace"],
       },
+      backgroundColor: {
+        primary: "#121828",
+      },
+      textColor: {
+        primary: "#fff",
+      },
     },
   },
   plugins: [],
