e25741e3b4483a08120ef2624dfcb9cecc995928
Paweł Gr <pawel.grzybek@l4web.pl>
2024-09-26 11:53:06 +0200

    feat(mobile-nav): add animation on nav wrapper

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -6,6 +6,26 @@ import { TbMenu2 } from "react-icons/tb";
 import { Link, useLocation } from "react-router-dom";
 import { useAccessibleNavigationPaths } from "../../router/utils";
 
+const navVariants = {
+  open: (height = 1000) => ({
+    clipPath: `circle(${height * 2 + 200}px at 50% -18px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(18px at 50% -18px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
+
 const MobileNav = () => {
   const [statusNav, toggleStatusNav] = useCycle(false, true);
   const accessiblePaths = useAccessibleNavigationPaths();
@@ -24,36 +44,38 @@ const MobileNav = () => {
         <span className="sr-only">Open mobile menu</span>
         {statusNav ? <IoMdClose /> : <TbMenu2 />}
       </motion.button>
-      {statusNav && (
-        <div className="fixed inset-0 bg-nav pt-32 p-6">
-          <div className="container mx-auto h-full grid grid-rows-[1fr_auto] gap-7">
-            <ul className="flex flex-col gap-7 pt-8">
-              {accessiblePaths.map(({ isVisibleOnNavigation, name, path }) =>
-                isVisibleOnNavigation ? (
-                  <li
-                    key={path}
-                    className={`${
-                      location.pathname === path && "text-red-600"
-                    } text-center font-bold text-3xl hover:text-red-600 transition-all`}
-                  >
-                    <Link className="block" to={path}>
-                      {name}
-                    </Link>
-                  </li>
-                ) : null
-              )}
-            </ul>
-            <div className="flex justify-center gap-5">
-              <Link to="https://github.com/pawel9911">
-                <ImGithub className="text-4xl" />
-              </Link>
-              <Link to="https://www.linkedin.com/in/pawel-grzybek/">
-                <FaLinkedin className="text-4xl" />
-              </Link>
-            </div>
+      <motion.div
+        animate={statusNav ? "open" : "closed"}
+        variants={navVariants}
+        className="fixed inset-0 bg-nav pt-32 p-6"
+      >
+        <div className="container mx-auto h-full grid grid-rows-[1fr_auto] gap-7">
+          <ul className="flex flex-col gap-7 pt-8">
+            {accessiblePaths.map(({ isVisibleOnNavigation, name, path }) =>
+              isVisibleOnNavigation ? (
+                <li
+                  key={path}
+                  className={`${
+                    location.pathname === path && "text-red-600"
+                  } text-center font-bold text-3xl hover:text-red-600 transition-all`}
+                >
+                  <Link className="block" to={path}>
+                    {name}
+                  </Link>
+                </li>
+              ) : null
+            )}
+          </ul>
+          <div className="flex justify-center gap-5">
+            <Link to="https://github.com/pawel9911">
+              <ImGithub className="text-4xl" />
+            </Link>
+            <Link to="https://www.linkedin.com/in/pawel-grzybek/">
+              <FaLinkedin className="text-4xl" />
+            </Link>
           </div>
         </div>
-      )}
+      </motion.div>
     </nav>
   );
 };
