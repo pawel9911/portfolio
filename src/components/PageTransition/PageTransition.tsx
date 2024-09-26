937fa1cc923d3bc6a487073b4b824da743e18b0b
Paweł Gr <pawel.grzybek@l4web.pl>
2024-09-26 16:40:58 +0200

    feat(mobile-nav): improvement of page transition

--- src/components/PageTransition/PageTransition.tsx
+++ src/components/PageTransition/PageTransition.tsx
@@ -10,37 +10,33 @@ const PageTransition = ({ children }: PageTransitionProps) => {
       {children}
       <AnimatePresence>
         <motion.div
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0, y: "-100%" }}
-          transition={{
-            delay: 0.9,
-            duration: 0.6,
-            ease: "easeInOut",
+          animate={{
+            clipPath: [
+              "polygon(0% 0%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%, 100% 100%, 100% 0%)",
+              "polygon(0% 0%, 0 50%, 100% 50%, 100% 50%, 0 50%, 0 100%, 100% 100%, 100% 0%)",
+              "polygon(0% 0%, 0 50%, 100% 50%, 100% 50%, 0 50%, 0 100%, 100% 100%, 100% 0%)",
+              "polygon(0% 0%, 0 0, 100% 0, 100% 100%, 0 100%, 0 100%, 100% 100%, 100% 0%)",
+            ],
           }}
-          className="h-[51vh] w-screen fixed bg-red-500 z-40 top-0"
-        />
-        <motion.div
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0, visibility: "hidden" }}
           transition={{
-            delay: 0.5,
-            duration: 0.4,
-            ease: "easeInOut",
+            duration: 2,
+            times: [0, 0.2, 0.8, 1],
           }}
-          className="text-7xl absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] z-50"
+          className="h-screen w-screen fixed bg-nav z-40 top-0"
         >
-          <GiSuperMushroom />
+          <motion.div
+            initial={{ opacity: 1 }}
+            animate={{ opacity: 0, visibility: "hidden" }}
+            transition={{
+              delay: 0.4,
+              duration: 1.2,
+              ease: "easeInOut",
+            }}
+            className="text-7xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50"
+          >
+            <GiSuperMushroom />
+          </motion.div>
         </motion.div>
-        <motion.div
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 0, y: "100%" }}
-          transition={{
-            delay: 0.9,
-            duration: 0.6,
-            ease: "easeInOut",
-          }}
-          className="h-[51vh] w-screen fixed bg-red-500 z-40 bottom-0"
-        />
       </AnimatePresence>
     </>
   );
