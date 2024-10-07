4fd535fca7d3110c82cbf6e292e29e58823006b6
Paweł Gr <pawel.grzybek@l4web.pl>
2024-10-07 14:26:42 +0200

    feat(contact-section): add env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_EMAIL_JS_SERVICE_ID: string;
+  readonly VITE_APP_EMAIL_JS_TEMPLATE_ID: string;
+  readonly VITE_APP_EMAIL_JS_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
