20809cc934dba0dd1c0e19dbeda887f4dd3ad939
Paweł Gr <pawel.grzybek@l4web.pl>
2024-12-02 13:58:19 +0100

    feat(resume-section): transfer stack section to  separated catalog

--- src/features/ResumeSection/ResumeSection.tsx
+++ src/features/ResumeSection/ResumeSection.tsx
@@ -1,9 +1,5 @@
 import { PageTransition } from "@components";
-import { stack } from "./utils";
-
-const gaps = "gap-8 lg:gap-7 xl:gap-10";
-const cols = "grid-cols-2 xs:grid-cols-3 md:grid-cols-4 lg:grid-cols-6";
-const padding = "p-6 xs:p-7 xl:p-8";
+import { StackSection } from "./components";
 
 const ResumeSection = () => {
   return (
@@ -21,19 +17,7 @@ const ResumeSection = () => {
           <h2 className="title">Education</h2>
         </section>
 
-        <section id="stack" className="mt-8 mb-16">
-          <h2 className="title">Stack</h2>
-          <ul className={`grid ${cols} ${gaps}`}>
-            {stack.map((e) => (
-              <li
-                className={`flex flex-col items-center rounded-lg shadow-item ${padding}`}
-              >
-                {e.icon}
-                <p className="h3 mt-1">{e.title}</p>
-              </li>
-            ))}
-          </ul>
-        </section>
+        <StackSection />
 
         <section id="experience" className="mt-8 mb-16">
           <h2 className="title">Experience</h2>
