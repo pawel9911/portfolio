5059bc84f88b63867373274807082d44f1faa3d0
Paweł Gr <pawel.grzybek@l4web.pl>
2024-12-02 13:42:21 +0100

    feat(resume-section): add sub section

--- src/features/ResumeSection/ResumeSection.tsx
+++ src/features/ResumeSection/ResumeSection.tsx
@@ -9,6 +9,18 @@ const ResumeSection = () => {
   return (
     <PageTransition>
       <div className="container text-9xl">
+        <section id="aboutMe" className="mt-8 mb-16">
+          <h2 className="title">About me</h2>
+        </section>
+
+        <section id="mySkills" className="mt-8 mb-16">
+          <h2 className="title">My skills</h2>
+        </section>
+
+        <section id="education" className="mt-8 mb-16">
+          <h2 className="title">Education</h2>
+        </section>
+
         <section id="stack" className="mt-8 mb-16">
           <h2 className="title">Stack</h2>
           <ul className={`grid ${cols} ${gaps}`}>
@@ -21,6 +33,14 @@ const ResumeSection = () => {
               </li>
             ))}
           </ul>
+        </section>
+
+        <section id="experience" className="mt-8 mb-16">
+          <h2 className="title">Experience</h2>
+        </section>
+
+        <section id="myProjects" className="mt-8 mb-16">
+          <h2 className="title">My projects</h2>
         </section>
       </div>
     </PageTransition>
