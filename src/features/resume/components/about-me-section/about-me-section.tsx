4cabf762265985ab9720fad28d3ce202797957ac
Paweł Gr <pawel.grzybek@l4web.pl>
2024-12-03 17:48:03 +0100

    feat(resume): add style for few images on about me section

--- src/features/resume/components/about-me-section/about-me-section.tsx
+++ src/features/resume/components/about-me-section/about-me-section.tsx
@@ -4,7 +4,6 @@ export const AboutMeSection = () => {
   return (
     <section id="aboutMe" className="mt-8 mb-16 max-w-4xl mx-auto">
       <h2 className="title">About me</h2>
-      {/* <div className="grid gap-20 shadow-item"> v2 */}
       <div className="grid gap-20">
         {passion.map((e, i) => {
           const isEven = i % 2 === 0;
@@ -12,33 +11,43 @@ export const AboutMeSection = () => {
           return (
             <div
               key={i}
-              // className={`flex gap-10 shadow-item  ${ // v3
               className={`flex gap-10 ${
                 isEven ? "flex-row" : "flex-row-reverse"
               }`}
             >
-              {e.images.map(({ src, signature }, id) => (
-                <div
-                  key={id}
-                  className={`p-6 pb-16 shadow-item shadow-black bg-white ${
-                    isEven
-                      ? // ? "-rotate-12 translate-x-14 translate-y-4" // v1
-                        "-rotate-12"
-                      : // : "rotate-12 -translate-x-14 translate-y-4" // v1
-                        "rotate-12"
-                  } `}
-                >
-                  <img
-                    src={src}
-                    alt="me"
-                    className="object-contain h-60 w-48 max-w-none bg-gray-100"
-                  />
-                  <p className="signature absolute bottom-6 right-7">
-                    {signature}
-                  </p>
+              <div className="w-full max-w-60 shrink-0">
+                <div className="relative">
+                  {e.images.map(({ src, signature }, id) => {
+                    const rotate = () => {
+                      switch (id) {
+                        case 0: {
+                          return isEven ? "-rotate-12" : "rotate-12";
+                        }
+
+                        default: {
+                          return "-z-10";
+                        }
+                      }
+                    };
+
+                    return (
+                      <div
+                        key={id}
+                        className={`p-6 pb-16 absolute shadow-item shadow-black bg-white ${rotate()}`}
+                      >
+                        <img
+                          src={src}
+                          alt="me"
+                          className="object-contain h-60 w-48 max-w-none bg-gray-100"
+                        />
+                        <p className="signature absolute bottom-6 right-7">
+                          {signature}
+                        </p>
+                      </div>
+                    );
+                  })}
                 </div>
-              ))}
-              {/* <div className="shadow-item"> v1 */}
+              </div>
               <div>
                 <p className="body1 p-10">{e.text}</p>
               </div>
