e5dd1800290b19ff4b68cb9d6a3e3dcfbcbbc06a
Paweł Gr <pawel.grzybek@l4web.pl>
2024-12-03 12:31:48 +0100

    feat(resume-section): add responsive for education

--- src/features/resume/components/education-section/education-section.tsx
+++ src/features/resume/components/education-section/education-section.tsx
@@ -6,12 +6,14 @@ export const EducationSection = () => {
       <h2 className="title">Education</h2>
       {education.map((e, i) => (
         <div key={i} className="flex flex-col items-center justify-center">
-          <h2 className="h3">{e.name}</h2>
-          <h3 className="h4 text-secondary mb-4">
+          <h2 className="h3 text-center">{e.name}</h2>
+          <h3 className="h4 text-center text-secondary mb-4">
             {e.date.from} — {e.date.to}
           </h3>
-          <h3 className="h4 mb-2">{e.profile}</h3>
-          <p className="body1 text-center w-1/2">{e.description}</p>
+          <h3 className="h4 text-center mb-2">{e.profile}</h3>
+          <p className="body1 text-center sm:w-3/4 md:w-2/3 lg:w-1/2">
+            {e.description}
+          </p>
         </div>
       ))}
     </section>
