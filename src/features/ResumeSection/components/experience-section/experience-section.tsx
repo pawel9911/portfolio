f5e2f547b784a8b001c57e3af0cfeac3727475d0
Paweł Gr <pawel.grzybek@l4web.pl>
2024-12-03 11:38:14 +0100

    feat(resume-section): add array with experience content

--- src/features/ResumeSection/components/experience-section/experience-section.tsx
+++ src/features/ResumeSection/components/experience-section/experience-section.tsx
@@ -1,70 +1,46 @@
 import { GiSuperMushroom } from "react-icons/gi";
+import { experience } from "./constants";
 
 export const ExperienceSection = () => {
   return (
     <section id="experience" className="mt-8 mb-16">
       <h2 className="title text-left">Experience</h2>
-
-      <div className="grid grid-cols-12 gap-2">
-        <div className="col-span-4">
-          <h3 className="h4">
-            April 2022 — <span className="text-secondary">Present</span>
-          </h3>
-        </div>
-        <div className="col-span-6">
-          <div className="mb-3">
-            <h2 className="h3">React Frontend Developer</h2>
-            <p className="body1">
-              L4WEB — <span className="text-secondary">Remote</span>
-            </p>
+      {experience.map((e) => (
+        <div key={e.id} className="grid grid-cols-12 gap-2 mb-10">
+          <div className="col-span-4">
+            <h3 className="h4">
+              {e.date.from} —{" "}
+              {e.date.to ? (
+                e.date.to
+              ) : (
+                <span className="text-secondary">Present</span>
+              )}
+            </h3>
           </div>
-          <p className="body1 mb-7 text-justify">
-            Dimension is a collaboration platform for modern engineering teams.
-            It bridges the gap between communication, cloud, code, projects, and
-            more—with an incredible developer experience.
-          </p>
-          <ul className="body1">
-            <li>
-              <div className="flex gap-2">
-                <div>
-                  <GiSuperMushroom className="text-secondary text-2xl " />
-                </div>
-                <p className="body1 mb-3 text-justify">
-                  Led the rewrite from the MVP version, which was previously
-                  fragile with numerous bugs and technical debt. Convinced the
-                  team to transition to a new monorepo project with a solid
-                  foundation, ensuring code quality.
-                </p>
-              </div>
-            </li>
-            <li>
-              <div className="flex gap-2">
-                <div>
-                  <GiSuperMushroom className="text-secondary text-2xl " />
-                </div>
-                <p className="body1 mb-3 text-justify">
-                  Led the transition of the application to a local-first setup
-                  using IndexedDB, significantly improving speed by reducing
-                  query and update times from about 500ms to nearly instant
-                  (around 5ms).
-                </p>
-              </div>
-            </li>
-            <li>
-              <div className="flex gap-2">
-                <div>
-                  <GiSuperMushroom className="text-secondary text-2xl " />
-                </div>
-                <p className="body1 mb-3 text-justify">
-                  Led the hiring process for software engineers, including
-                  designing interview processes and evaluating candidates to
-                  build a strong development team.
-                </p>
-              </div>
-            </li>
-          </ul>
+          <div className="col-span-6">
+            <div className="mb-3">
+              <h2 className="h3">{e.position}</h2>
+              <p className="body1">
+                {e.company} —{" "}
+                <span className="text-secondary">{e.workType}</span>
+              </p>
+            </div>
+            <p className="body1 mb-7 text-justify">{e.description}</p>
+            <ul className="body1">
+              {e.list.map((text, i) => (
+                <li key={i}>
+                  <div className="flex gap-2">
+                    <div>
+                      <GiSuperMushroom className="text-secondary text-2xl " />
+                    </div>
+                    <p className="body1 mb-3 text-justify">{text}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
+      ))}
     </section>
   );
 };
