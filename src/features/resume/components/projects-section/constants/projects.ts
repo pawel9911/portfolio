b7d35d14f136dc855cd921994db32041723793e4
Paweł Gr <pawel.grzybek@l4web.pl>
2024-12-18 17:42:01 +0100

    feat(resume): add projects

---
+++ src/features/resume/components/projects-section/constants/projects.ts
@@ -0,0 +1,50 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Instytut Matki i dziecka",
+    category: "Aplikacja Webowa",
+    tools: [
+      "Express",
+      "MongoDB",
+      "OpenAI API",
+      "AWS SES",
+      "AWS S3",
+      "Node Mailer",
+      "Joi",
+    ],
+    description:
+      "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset.",
+  },
+  {
+    id: 2,
+    name: "Instytut Matki i dziecka",
+    category: "Aplikacja Webowa",
+    tools: [
+      "Express",
+      "MongoDB",
+      "OpenAI API",
+      "AWS SES",
+      "AWS S3",
+      "Node Mailer",
+      "Joi",
+    ],
+    description:
+      "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset.",
+  },
+  {
+    id: 3,
+    name: "Instytut Matki i dziecka",
+    category: "Aplikacja Webowa",
+    tools: [
+      "Express",
+      "MongoDB",
+      "OpenAI API",
+      "AWS SES",
+      "AWS S3",
+      "Node Mailer",
+      "Joi",
+    ],
+    description:
+      "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset.",
+  },
+];
