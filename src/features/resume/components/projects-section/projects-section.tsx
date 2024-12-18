2e1a25329d5207ac663110b574555ca31f56609c
Paweł Gr <pawel.grzybek@l4web.pl>
2024-12-18 17:44:07 +0100

    feat(resume): use project on project section

--- src/features/resume/components/projects-section/projects-section.tsx
+++ src/features/resume/components/projects-section/projects-section.tsx
@@ -1,7 +1,20 @@
+import { projects } from "./constants";
+import { Project } from "./project";
+
 export const ProjectsSection = () => {
   return (
     <section id="myProjects" className="mt-8 mb-16">
       <h2 className="title">My projects</h2>
+      <h3 className="h3 mb-10 text-center">
+        Here you will find some of the personal and clients projects that I
+        created with each project containing its own case study
+      </h3>
+
+      <ul className="grid grid-cols-3 gap-10">
+        {projects.map((data) => (
+          <Project data={data} />
+        ))}
+      </ul>
     </section>
   );
 };
