058c34c1d246fd205e22e011b79108306f9c7eee
Paweł Gr <pawel.grzybek@l4web.pl>
2025-01-12 21:08:36 +0100

    feat(content): add experience description

--- src/features/resume/components/experience-section/constants/experience.ts
+++ src/features/resume/components/experience-section/constants/experience.ts
@@ -8,12 +8,14 @@ export const experience = [
     position: "React Frontend Developer",
     company: "L4WEB",
     workType: "Remote",
-    description:
-      "Dimension is a collaboration platform for modern engineering teams. It bridges the gap between communication, cloud, code, projects, and more—with an incredible developer experience.",
+    description: "", // todo add something
     list: [
-      "Led the rewrite from the MVP version, which was previously fragile with numerous bugs and technical debt. Convinced the team to transition to a new monorepo project with a solid foundation, ensuring code quality.",
-      "Led the transition of the application to a local-first setup using IndexedDB, significantly improving speed by reducing query and update times from about 500ms to nearly instant (around 5ms).",
-      "Led the hiring process for software engineers, including designing interview processes and evaluating candidates to build a strong development team.",
+      "Developing well architected and scalable solutions using React",
+      "Maintaining quality, performance and security of created solutions",
+      "Connecting Frontend with Backend using REST APIs and WebSockets",
+      "Estimating time needed to implement new features",
+      "Conducting technical part of interview for JavaScript / TypeScript roles",
+      "Communication with clients",
     ],
   },
 ];
