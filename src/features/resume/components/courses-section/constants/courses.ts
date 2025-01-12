276cbe5578e900023f4e43545a830a2fda01d4c5
Paweł Gr <pawel.grzybek@l4web.pl>
2025-01-12 19:24:23 +0100

    feat(content): add courses in English

--- src/features/resume/components/courses-section/constants/courses.ts
+++ src/features/resume/components/courses-section/constants/courses.ts
@@ -3,8 +3,11 @@ export const courses = [
     name: "Coders Lab",
     date: { from: "April 2020", to: "October 2020" },
     profile: "JavaScript Developer: React",
+    /* Kurs zawierał 240 godzin wykładów oraz zajęć praktycznych z
+       wykładowcą i obejmuje zakres wiedzy wymagany na stanowisku
+       Junior Front-End Developer. */
     description:
-      "Kurs zawierał 240 godzin wykładów oraz zajęć praktycznych z wykładowcą i obejmuje zakres wiedzy wymagany na stanowisku Junior Front-End Developer.",
+      "The course included 240 hours of lectures and practical sessions with the instructor and covers the knowledge required for the position of Junior Front-End Developer.",
     tags: [
       "Git",
       "HTML",
