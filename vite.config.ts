322108c3694a40b1d01817b7efd95e727eb87b1e
Paweł Gr <pawel.grzybek@l4web.pl>
2026-04-21 10:51:08 +0200

    Fix asset paths by environment mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: "/portfolio",
-  server: {
-    port: 4300,
-    host: "localhost",
-  },
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tsconfigPaths()],
+    base: mode === "production" ? "/portfolio/" : "/",
+    server: {
+      port: 4300,
+      host: "localhost",
+    },
+  };
 });
